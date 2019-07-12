6431a3bb0f8927c7e571c69bddd93cf4e0178cee
mikolaslorant <lmikolas@itba.edu.ar>
2019-07-12 13:56:52 -0300

    added book create endpoint

--- training/src/main/java/wolox/training/controllers/BookController.java
+++ training/src/main/java/wolox/training/controllers/BookController.java
@@ -1,12 +1,29 @@
 package wolox.training.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import wolox.training.models.Book;
+import wolox.training.repositories.BookRepository;
 
-@Controller
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @PostMapping
+    public Book create(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
     @GetMapping
     public String greeting(@RequestParam(name="name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
