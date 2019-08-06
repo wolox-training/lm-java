1fa5152c967fc01ab2a79f40491e5911e026a2d3
mikolaslorant <lmikolas@itba.edu.ar>
2019-08-06 10:11:26 -0300

    final added to message constants class and constructor

--- training/src/main/java/wolox/training/utils/MessageConstants.java
+++ training/src/main/java/wolox/training/utils/MessageConstants.java
@@ -1,9 +1,13 @@
 package wolox.training.utils;
 
-public class MessageConstants {
+public final class MessageConstants {
     public static final String NULL_PARAMETER = "The parameter should not be null.";
 
     public static final String EMPTY_STRING = "String should have at least 1 character.";
 
     public static final String NEGATIVE_QUANTITY = "The quantity should be a positive integer.";
+
+    private MessageConstants() {
+        // This constructor is defined to avoid the instantiation of this class
+    }
 }
