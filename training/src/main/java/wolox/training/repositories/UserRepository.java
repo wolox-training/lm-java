a0506c8690fb29d8a2ae73bf438b2292fe982510
mikolaslorant <lmikolas@itba.edu.ar>
2019-08-28 16:06:23 -0300

    improved queries making parameters not required and using JPA Query

--- training/src/main/java/wolox/training/repositories/UserRepository.java
+++ training/src/main/java/wolox/training/repositories/UserRepository.java
@@ -3,10 +3,15 @@ package wolox.training.repositories;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import wolox.training.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findFirstByUsername(String username);
-    List<User> findByBirthdateBetweenAndNameContainingIgnoreCase(LocalDate start, LocalDate end, String name);
+    @Query("SELECT u FROM User u WHERE (CAST(:startDate AS date) IS NULL OR u.birthdate >= :startDate)"
+        + " AND (CAST(:endDate AS date) IS NULL OR u.birthdate <= :endDate)"
+        + " AND (:name IS NULL OR LOWER(u.name) LIKE %:name%)")
+    List<User> findByBirthdateBetweenAndNameContainingIgnoreCase(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("name") String name);
 }
