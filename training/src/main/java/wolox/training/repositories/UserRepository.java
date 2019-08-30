1417ec4d3e352d206cb7eae29060d3e92fa50c27
mikolaslorant <lmikolas@itba.edu.ar>
2019-08-30 09:28:59 -0300

    added paging and sorting in user controller and book controller (and corresponding repositories)

--- training/src/main/java/wolox/training/repositories/UserRepository.java
+++ training/src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,24 +1,25 @@
 package wolox.training.repositories;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import wolox.training.models.User;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
     Optional<User> findFirstByUsername(String username);
 
     @Query("SELECT u FROM User u WHERE (CAST(:startDate AS date) IS NULL OR u.birthdate >= :startDate)"
         + " AND (CAST(:endDate AS date) IS NULL OR u.birthdate <= :endDate)"
         + " AND (:name IS NULL OR LOWER(u.name) LIKE %:name%)"
         + " AND (:username IS NULL OR LOWER(u.username) LIKE %:username%)")
-    List<User> findAll(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("name") String name, @Param("username") String username);
+    Page<User> findAll(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("name") String name, @Param("username") String username, Pageable pageable);
 
     @Query("SELECT u FROM User u WHERE (CAST(:startDate AS date) IS NULL OR u.birthdate >= :startDate)"
         + " AND (CAST(:endDate AS date) IS NULL OR u.birthdate <= :endDate)"
         + " AND (:name IS NULL OR LOWER(u.name) LIKE %:name%)")
-    List<User> findByBirthdateBetweenAndNameContainingIgnoreCase(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("name") String name);
+    Page<User> findByBirthdateBetweenAndNameContainingIgnoreCase(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("name") String name, Pageable pageable);
 }
